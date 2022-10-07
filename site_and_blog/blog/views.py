69ceb5506dadd48d3c48b5b48dae7b0d607120b6
wes <wal2.designer@gmail.com>
2022-10-07 20:16:31 +0100

    starging blog

---
+++ site_and_blog/blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+# TODO: / load starting page with list latests block post and some welcome text
+# TODO: /post  load page with list all blog posts
+# TODO: /post/<slug> load individual blog post page with shows full  blog post
