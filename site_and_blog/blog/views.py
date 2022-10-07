573fa486c9ab02d026a4b85dbcf3c1d7a68118bd
wes <wal2.designer@gmail.com>
2022-10-07 20:35:04 +0100

    index view and urls

--- site_and_blog/blog/views.py
+++ site_and_blog/blog/views.py
@@ -1,6 +1,14 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
 # TODO: / load starting page with list latests block post and some welcome text
 # TODO: /post  load page with list all blog posts
 # TODO: /post/<slug> load individual blog post page with shows full  blog post
+
+class Index (View):
+    
+    def get(self, request):
+        return HttpResponse('index')
