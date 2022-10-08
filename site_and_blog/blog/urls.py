092c69539c3a371c5f26c8faea1cb34500b12dff
wes <wal2.designer@gmail.com>
2022-10-08 08:53:35 +0100

    adding names to urls

--- site_and_blog/blog/urls.py
+++ site_and_blog/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import Index, Posts
 
 urlpatterns = [
-    path('', Index.as_view()),
-    path('posts/', Posts.as_view()),
-    path('posts/<str:slug>', Posts.as_view())
+    path('', Index.as_view(), name='starting-page'),
+    path('posts/', Posts.as_view(), name='posts-page'),
+    path('posts/<slug:slug>', Posts.as_view(), name='post-detail-page')
 ]
