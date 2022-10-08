000cb52b2b455670a6cd1f7b67657233301b20db
wes <wal2.designer@gmail.com>
2022-10-08 12:41:07 +0100

    starting with post detail page

--- site_and_blog/blog/urls.py
+++ site_and_blog/blog/urls.py
@@ -3,6 +3,6 @@ from .views import Index, Posts
 
 urlpatterns = [
     path('', Index.as_view(), name='starting-page'),
-    path('posts/', Posts.as_view(), name='posts-page'),
+    path('posts/', Posts.as_view(), name='post-page'),
     path('posts/<slug:slug>', Posts.as_view(), name='post-detail-page')
 ]
