00ec4076445f25fc3ed8a1759eb56fd3a2555556
wes <wal2.designer@gmail.com>
2022-10-11 09:00:24 +0100

    creating migrations

--- book_shelf/book_store/models.py
+++ book_shelf/book_store/models.py
@@ -6,6 +6,5 @@ import uuid
 # Create your models here.
 
 class Book(models.Model):
-    id = models.UUIDField(primery_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100)
     rating = models.IntegerField()
