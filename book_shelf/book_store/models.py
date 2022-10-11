9ae8a62a0e0d528b74d63451ce14c77844cb5d3d
wes <wal2.designer@gmail.com>
2022-10-11 08:53:27 +0100

    adding uuid as id

--- book_shelf/book_store/models.py
+++ book_shelf/book_store/models.py
@@ -1,8 +1,11 @@
+from email.policy import default
 from turtle import title
 from django.db import models
+import uuid
 
 # Create your models here.
 
 class Book(models.Model):
+    id = models.UUIDField(primery_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100)
     rating = models.IntegerField()
