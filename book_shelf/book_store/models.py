00db6493809b93cd797b070238cc098b2812b585
wes <wal2.designer@gmail.com>
2022-10-12 20:13:46 +0100

    adding validators

--- book_shelf/book_store/models.py
+++ book_shelf/book_store/models.py
@@ -1,13 +1,14 @@
 from email.policy import default
 from turtle import title
+from wsgiref.validate import validator
 from django.db import models
-import uuid
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
 
     def __str__(self):
         return f"{self.title} ({self.rating})" #function to format string return from model
