625d3ad14cb449c7a1743ece50ae89eaca40b012
wes <wal2.designer@gmail.com>
2022-10-12 20:10:58 +0100

    change str

--- book_shelf/book_store/models.py
+++ book_shelf/book_store/models.py
@@ -10,4 +10,4 @@ class Book(models.Model):
     rating = models.IntegerField()
 
     def __str__(self):
-        return f"book:{self.title}, rating:{self.rating}" #function to format string return from model
+        return f"{self.title} ({self.rating})" #function to format string return from model
