fdc94fcf1124fbccbed06cd66b4e123003049886
Wes <wal2.designer@gmail.com>
2022-11-07 08:44:58 +0000

    a lot of changes

--- site_and_blog/blog/views.py
+++ site_and_blog/blog/views.py
@@ -1,46 +1,28 @@
 from datetime import date
 from http.client import HTTPResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import View
 
-all_posts = [
-    {
-    "slug" : "hike-in-the-mountains",
-    "image" : 'mountains.jpg',
-    "author": "Max",
-    "date" : date(2022, 10, 8),
-    "title": "Hiking mountains",
-    "excerpt": 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur saepe repudiandae officiis delectus illum. Dignissimos vel praesentium, voluptas officia laborum minus fugiat dolore iure recusandae iste, in maxime, molestias natus.',
-    "content": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur saepe repudiandae officiis delectus illum. Dignissimos vel praesentium, voluptas officia laborum minus fugiat dolore iure recusandae iste, in maxime, molestias natus."
-},{
-    "slug" : "hike-in-the-mountains-2",
-    "image" : 'mountains.jpg',
-    "author": "Max",
-    "date" : date(2022, 10, 8),
-    "title": "Hiking mountains 2",
-    "excerpt": 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur saepe repudiandae officiis delectus illum. Dignissimos vel praesentium, voluptas officia laborum minus fugiat dolore iure recusandae iste, in maxime, molestias natus.',
-    "content": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur saepe repudiandae officiis delectus illum. Dignissimos vel praesentium, voluptas officia laborum minus fugiat dolore iure recusandae iste, in maxime, molestias natus."
-}
-]
-
-# Create your views here.
-
-#helper temporaryly
-def get_date(post):
-    return post['date']
-
-def Index (request):
-    sorted_post = sorted(all_posts, key=get_date)
-    latest_post = sorted_post[-3:]
-
-    return render(request, "blog/index.html", context={ "posts" : latest_post })
+from .models import Post
 
 
-def Posts (request):
-    return render(request, "blog/all-post.html", context={'all_posts' : all_posts})
+def Index(request):
+    latest_post = Post.objects.all().order_by("-created_date")[:3]
+
+    return render(request, "blog/index.html", context={"posts": latest_post})
 
 
-def Posts_details (request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
-    return render(request, "blog/post-detail.html", context={"post": identified_post})
+def Posts(request):
+    all_posts = Post.objects.all()
+    return render(request,
+                  "blog/all-post.html",
+                  context={'all_posts': all_posts})
+
+
+def Posts_details(request, slug):
+    identified_post = get_object_or_404(Post, slug == slug)
+    # identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request,
+                  "blog/post-detail.html",
+                  context={"post": identified_post})
