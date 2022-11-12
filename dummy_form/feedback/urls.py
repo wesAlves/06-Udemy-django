5eb0e0ef3b7ac042aec31b6f34c31e6fb3a1e11a
Wes <wal2.designer@gmail.com>
2022-11-12 08:33:46 +0000

    adding form and redirect page

--- dummy_form/feedback/urls.py
+++ dummy_form/feedback/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.reviews)]
+urlpatterns = [
+    path("", views.reviews, name='reviews'),
+    path("thank-you/", views.thank_you)
+]
--- dummy_form/feedback/views.py
+++ dummy_form/feedback/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
 def reviews(request):
+
+    if request.method == 'POST':
+        entered_username = request.POST['username']
+        print(entered_username)
+        return HttpResponseRedirect("/reviews/thank-you")
+
     return render(request, 'feedback/feedback.html')
+
+
+def thank_you(request):
+    return render(request, "feedback/thank_you.html")
