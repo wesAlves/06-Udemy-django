37bbd1437d1b39876e20be60a2980aceb275af11
Wes <wal2.designer@gmail.com>
2022-11-13 12:11:52 +0000

    adding some extras to form model

--- dummy_form/feedback/forms.py
+++ dummy_form/feedback/forms.py
@@ -2,22 +2,16 @@ from django import forms
 
 from .models import Review
 
-# class ReviewForm(forms.Form):
-#     user_name = forms.CharField(
-#         label="You name",
-#         max_length=100,
-#         error_messages={
-#             'required': 'you name must be filled',
-#             "max_length": "The maximu length for name is 100 characters"
-#         })
-#     review_text = forms.CharField(label="Your feedback",
-#                                   widget=forms.Textarea,
-#                                   max_length=500)
-#     rating = forms.IntegerField(max_value=5)
-
 
 class ReviewForm(forms.ModelForm):
 
     class Meta:
         model = Review
         fields = "__all__"
+        labels = {"user_name": "Your name", "review_text": "Your feedback"}
+
+        error_messages = {
+            "user_name": {
+                "required": "This field is required!!!!"
+            }
+        }
