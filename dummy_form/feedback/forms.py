00cc42eb7bfd49b3c7bad26a64bdd14f58f15527
Wes <wal2.designer@gmail.com>
2022-11-13 12:08:13 +0000

    form generated from model

--- dummy_form/feedback/forms.py
+++ dummy_form/feedback/forms.py
@@ -1,15 +1,23 @@
 from django import forms
 
+from .models import Review
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(
-        label="You name",
-        max_length=100,
-        error_messages={
-            'required': 'you name must be filled',
-            "max_length": "The maximu length for name is 100 characters"
-        })
-    review_text = forms.CharField(label="Your feedback",
-                                  widget=forms.Textarea,
-                                  max_length=500)
-    rating = forms.IntegerField(max_value=5)
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(
+#         label="You name",
+#         max_length=100,
+#         error_messages={
+#             'required': 'you name must be filled',
+#             "max_length": "The maximu length for name is 100 characters"
+#         })
+#     review_text = forms.CharField(label="Your feedback",
+#                                   widget=forms.Textarea,
+#                                   max_length=500)
+#     rating = forms.IntegerField(max_value=5)
+
+
+class ReviewForm(forms.ModelForm):
+
+    class Meta:
+        model = Review
+        fields = "__all__"
