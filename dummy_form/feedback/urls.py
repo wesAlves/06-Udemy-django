26be6d9ae47308ce76a5570752efb52559c43547
Wes <wal2.designer@gmail.com>
2022-11-14 09:03:21 +0000

    adding some templates extra features

--- dummy_form/feedback/urls.py
+++ dummy_form/feedback/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.reviews, name='reviews'),
+    path("", views.ReviewView.as_view(), name='reviews'),
     path("thank-you/", views.thank_you)
 ]
--- dummy_form/feedback/views.py
+++ dummy_form/feedback/views.py
@@ -1,31 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views import View
 
 from .forms import ReviewForm
 from .models import Review
 
-
 # Create your views here.
-def reviews(request):
-    if request.method == 'POST':
+
+
+class ReviewView(View):
+
+    def get(self, request):
+        form = ReviewForm()
+
+        return render(request, 'feedback/feedback.html', {"form": form})
+
+    def post(self, request):
         form = ReviewForm(request.POST)
 
         if form.is_valid():
-            # print(form.cleaned_data)
-            review = Review(
-                user_name=form.cleaned_data['user_name'],
-                review_text=form.cleaned_data['review_text'],
-                rating=form.cleaned_data['rating'],
-            )
-
-            review.save()
-
+            form.save()
             return HttpResponseRedirect("/reviews/thank-you")
 
-    else:
-        form = ReviewForm()
 
-    return render(request, 'feedback/feedback.html', {"form": form})
+# def reviews(request):
+#     if request.method == 'POST':
+#         form = ReviewForm(request.POST)
+
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect("/reviews/thank-you")
+
+#     else:
+#         form = ReviewForm()
+
+#     return render(request, 'feedback/feedback.html', {"form": form})
 
 
 def thank_you(request):
