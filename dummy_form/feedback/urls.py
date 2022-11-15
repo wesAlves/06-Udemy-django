395e183a4c0a29c90c99d20d99d9a162d4667dcf
Wes <wal2.designer@gmail.com>
2022-11-15 08:48:27 +0000

    change some files

--- dummy_form/feedback/urls.py
+++ dummy_form/feedback/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.ReviewView.as_view(), name='reviews'),
+    path("all/", views.ReviewView.as_view(), name='reviews_list'),
     path("thank-you/", views.thank_you)
 ]
--- dummy_form/feedback/views.py
+++ dummy_form/feedback/views.py
@@ -23,19 +23,5 @@ class ReviewView(View):
             return HttpResponseRedirect("/reviews/thank-you")
 
 
-# def reviews(request):
-#     if request.method == 'POST':
-#         form = ReviewForm(request.POST)
-
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect("/reviews/thank-you")
-
-#     else:
-#         form = ReviewForm()
-
-#     return render(request, 'feedback/feedback.html', {"form": form})
-
-
 def thank_you(request):
     return render(request, "feedback/thank_you.html")
