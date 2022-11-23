659eedd85c98ef288c4a2edbd3ea06ed0d69a9c8
Wes <wal2.designer@gmail.com>
2022-11-23 08:27:52 +0000

    handle upload with django

--- dummy_form/profiles/views.py
+++ dummy_form/profiles/views.py
@@ -2,21 +2,30 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 
+from .models import UserProfile
+from .forms import ProfileForm
+
 # Create your views here.
 
 
 class CreateProfileView(View):
 
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {"form": form})
 
     def post(self, request):
-        store_file(request.FILES["image"])
-
+        profile = UserProfile(image=request.FILES["user_image"])
+        profile.save()
         return HttpResponseRedirect("/profiles")
 
 
-def store_file(file):
-    with open("temp/image.jpg", "wb+") as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
+# def store_file(self, request):
+#     submitted_form = ProfileForm(request.POST, request.FILES)
+
+#     if submitted_form.is_valid():
+#         store_file(request.FILES["image"])
+#         return HttpResponseRedirect('/profiles')
+
+#     return render(request, "profiles/create_profile.html",
+#                   {"form": submitted_form})
